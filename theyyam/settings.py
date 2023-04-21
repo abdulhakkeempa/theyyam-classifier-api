d2d8b788e9f12aff2180916663fa753bb7cdf1cc
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-04-21 21:15:45 +0530

    added static root

--- theyyam/settings.py
+++ theyyam/settings.py
@@ -124,6 +124,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+# For storing the media files.
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+
+STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
 
 # Default primary key field type
